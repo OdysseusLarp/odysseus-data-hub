8cc13e740e95bc9b505796ae2df20b183bc10d07
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-01-05 19:36:07 +0200

    feat: example of limiting access to certain data to certain person groups

--- src/app/components/personnel-details/personnel-details.component.ts
+++ src/app/components/personnel-details/personnel-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { getPersonId } from '@api/Person';
+import { Observable } from 'rxjs';
+import { StateService } from '@app/services/state.service';
 
 @Component({
 	selector: 'app-personnel-details',
@@ -8,10 +10,12 @@ import { getPersonId } from '@api/Person';
 	styleUrls: ['./personnel-details.component.scss'],
 })
 export class PersonnelDetailsComponent implements OnInit {
+	user$: Observable<api.Person>;
 	person: api.Person;
-	constructor(private route: ActivatedRoute) {}
+	constructor(private state: StateService, private route: ActivatedRoute) {}
 
 	ngOnInit() {
+		this.user$ = this.state.user;
 		this.route.params.subscribe(({ id }) =>
 			getPersonId(id).then((res: api.Response<any>) => (this.person = res.data))
 		);

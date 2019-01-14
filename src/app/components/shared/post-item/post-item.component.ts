1a960570f10c952928b1eddf613da7262803de80
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-01-14 23:54:09 +0200

    fix: make build pass, fix type issue, lock core-decorators version

--- src/app/components/shared/post-item/post-item.component.ts
+++ src/app/components/shared/post-item/post-item.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+interface Post extends api.Post {
+	author: api.Person;
+}
+
 @Component({
 	selector: 'app-post-item',
 	templateUrl: './post-item.component.html',
 	styleUrls: ['./post-item.component.scss'],
 })
 export class PostItemComponent implements OnInit {
-	@Input() post: api.Post;
+	@Input() post: Post;
 	constructor() {}
 
 	ngOnInit() {}

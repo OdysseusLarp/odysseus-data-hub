f645eccc25cb7b08e4046b50e7b13f401a2c1dab
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-03-19 21:13:53 +0200

    feat: add simple form for adding new medical entries

--- src/app/components/personnel-details/personnel-details.component.ts
+++ src/app/components/personnel-details/personnel-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { getPersonId } from '@api/Person';
+import { getPersonId, postPersonIdMedicalEntry } from '@api/Person';
+import * as moment from 'moment';
 
 @Component({
 	selector: 'app-personnel-details',
@@ -8,12 +9,31 @@ import { getPersonId } from '@api/Person';
 	styleUrls: ['./personnel-details.component.scss'],
 })
 export class PersonnelDetailsComponent implements OnInit {
+	@ViewChild('medicalEntryForm') medicalEntryForm: ElementRef;
 	person: api.Person;
+	isSubmitting = false;
 	constructor(private route: ActivatedRoute) {}
 
 	ngOnInit() {
-		this.route.params.subscribe(({ id }) =>
-			getPersonId(id).then((res: api.Response<any>) => (this.person = res.data))
-		);
+		this.route.params.subscribe(({ id }) => this.fetchPerson(id));
+	}
+
+	private fetchPerson(id) {
+		getPersonId(id).then((res: api.Response<any>) => (this.person = res.data));
+	}
+
+	saveMedicalEntry() {
+		const value = this.medicalEntryForm.nativeElement.value;
+		if (!value || !this.person || this.isSubmitting) return;
+		this.isSubmitting = true;
+		postPersonIdMedicalEntry(this.person.id, {
+			// TODO: Convert time to fictional year or get rid of it
+			time: moment().format('D.M.YYYY'),
+			details: value,
+		}).then(() => {
+			this.fetchPerson(this.person.id);
+			this.isSubmitting = false;
+			this.medicalEntryForm.nativeElement.value = '';
+		});
 	}
 }

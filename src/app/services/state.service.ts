a7249e04fa0dc7acd18b608da0a40bd39481321f
Nico Hagelberg <nico.hagelberg@gmail.com>
2019-06-04 19:32:16 +0300

    feat: audit logging for logins

--- src/app/services/state.service.ts
+++ src/app/services/state.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject, Subject } from 'rxjs';
 import { get } from 'lodash';
 import { getPersonCardId, getPersonId } from '@api/Person';
 import { getDataTypeId } from '@api/Data';
+import { postLogAudit } from '@api/Log';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,7 +11,7 @@ import { getDataTypeId } from '@api/Data';
 export class StateService {
 	user: BehaviorSubject<api.Person> = new BehaviorSubject(null);
 	sessionStorage = window.sessionStorage;
-	logout = new Subject<api.Person>();
+	logout = new Subject<void>();
 	canEnableHacking$ = new BehaviorSubject(false);
 	hackingTarget$ = new BehaviorSubject(null);
 	showHackingView = new BehaviorSubject(false);
@@ -20,7 +21,7 @@ export class StateService {
 	constructor() {
 		// Attempt to log in as previous user automatically
 		const previousUserId = this.sessionStorage.getItem('previousUserId');
-		if (previousUserId) this.login(previousUserId);
+		if (previousUserId) this.login(previousUserId, true);
 		else this.hasInitialized$.next(true);
 
 		getDataTypeId('metadata', 'ship').then(res => {
@@ -30,13 +31,15 @@ export class StateService {
 
 		// Handle logout
 		this.logout.subscribe(() => {
+			const person_id = get(this.user.getValue(), 'id');
+			if (person_id) postLogAudit({ person_id, type: 'LOGOUT' });
 			this.sessionStorage.removeItem('previousUserId');
 			this.user.next(null);
 		});
 	}
 
-	login(id): Promise<api.Person> {
-		return getPersonCardId(id)
+	login(id, isRestoredSession = false): Promise<api.Person> {
+		return getPersonCardId(id, { login: !isRestoredSession })
 			.then((res: api.Response<any>) => {
 				if (!res.data) throw new Error('User not found');
 				this.user.next(res.data);
@@ -47,10 +50,9 @@ export class StateService {
 	}
 
 	loginHacker(id): Promise<api.Person> {
+		const hackerId = get(this.user.getValue(), 'id');
 		this.logout.next();
-		// TODO: Send audit log message
-		// const hackerId = this.user.getValue().id;
-		return getPersonId(id)
+		return getPersonId(id, { login: true, hackerId })
 			.then((res: api.Response<any>) => {
 				if (!res.data) throw new Error('User not found');
 				this.user.next(res.data);

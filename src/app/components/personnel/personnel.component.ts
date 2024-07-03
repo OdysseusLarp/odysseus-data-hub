4372bff2d815426f7f67c656f0c953d26f5a3ddf
Nico Hagelberg <nicou@nicou.me>
2024-07-03 20:42:29 +0300

    Display political party in person list

--- src/app/components/personnel/personnel.component.ts
+++ src/app/components/personnel/personnel.component.ts
@@ -125,12 +125,19 @@ export class PersonnelComponent implements OnInit {
 				prop: 'full_name',
 				name: 'Name',
 				cellTemplate: this.nameTemplate,
-				width: getWidth(27),
+				width: getWidth(22),
 				...columnSettings,
 			},
 			{
 				prop: 'dynasty',
 				name: 'Dynasty',
+				...columnSettings,
+				width: getWidth(12),
+			},
+			{
+				// political party
+				prop: 'political_party',
+				name: 'Party',
 				...columnSettings,
 				width: getWidth(12),
 			},
@@ -150,7 +157,7 @@ export class PersonnelComponent implements OnInit {
 			{
 				prop: 'status',
 				name: 'Status',
-				width: getWidth(26),
+				width: getWidth(22),
 				...columnSettings,
 			},
 		];

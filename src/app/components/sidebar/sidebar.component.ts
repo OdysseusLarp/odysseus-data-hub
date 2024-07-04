515e00a297687a528860df604442cb03994744a4
Nico Hagelberg <nicou@nicou.me>
2024-07-04 08:08:35 +0300

    Add notification sound for group chat account comms
    When group chat accounts receive new messages, play a notification
    sound until the message has been read.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	ElementRef,
+	OnDestroy,
+	OnInit,
+	ViewChild,
+} from '@angular/core';
 import { StateService } from '@app/services/state.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { MessagingService } from '@services/messaging.service';
 import { PermissionService } from '@app/services/permission.service';
+import { filter, pairwise } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-sidebar',
 	templateUrl: './sidebar.component.html',
 	styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
+	@ViewChild('messageNotificationAudio') notificationAudioEl: ElementRef;
 	user$: Observable<api.Person>;
 	unseenMessagesCount$: Observable<number>;
+	subscriptions = new Subscription();
 
 	constructor(
 		private state: StateService,
@@ -24,11 +33,52 @@ export class SidebarComponent implements OnInit {
 	ngOnInit() {
 		this.user$ = this.state.user;
 		this.unseenMessagesCount$ = this.messaging.unseenMessagesCount;
+
+		const user = this.state.user.getValue();
+		const isGroupChatAccount = user
+			? user.character_group === 'Group Chat'
+			: false;
+
+		if (isGroupChatAccount) {
+			const playSubscription = this.unseenMessagesCount$
+				.pipe(
+					pairwise(),
+					filter(
+						([prevCount, currentCount]) => prevCount === 0 && currentCount > 0
+					)
+				)
+				.subscribe(() => {
+					this.playNotificationSound();
+				});
+
+			const pauseSubscription = this.unseenMessagesCount$
+				.pipe(filter(count => count === 0))
+				.subscribe(() => {
+					this.pauseNotificationSound();
+				});
+
+			this.subscriptions.add(playSubscription);
+			this.subscriptions.add(pauseSubscription);
+		}
+	}
+
+	ngOnDestroy() {
+		this.pauseNotificationSound();
+		this.subscriptions.unsubscribe();
 	}
 
 	onLogout() {
 		this.state.logout.next();
 		this.router.navigate(['/']);
+	}
+
+	playNotificationSound() {
+		this.notificationAudioEl.nativeElement.currentTime = 0;
+		this.notificationAudioEl.nativeElement.play();
+	}
+
+	pauseNotificationSound() {
+		this.notificationAudioEl.nativeElement.pause();
 	}
 
 	openUserProfile() {
